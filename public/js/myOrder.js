0dba26ad1381e19de120ac5914a51992d59aac9f
tony <tzw442033370@live.com>
2017-08-01 14:51:03 +0800

    进入我的订单，从后台获取订单数据

--- public/js/myOrder.js
+++ public/js/myOrder.js
@@ -1,4 +1,45 @@
+/*jshint esversion: 6 */
 $(function () {
 	$("#top>div").load("header.html");
 	$("#footer").load("footer.html");
+
+
+	//表单获取
+	(function () {
+		var uid = sessionStorage.getItem("uid");
+		console.log(uid);
+		$.ajax({
+			url: "/myOrder",
+			data: {
+				uid: uid
+			},
+			success: function (result) {
+				if (result.code == -1) {
+					toastr.error(result.msg);
+				} else {
+					console.log(result.cxt);
+					var obj = result.cxt;
+					var html = "";
+					for (var i = 0; i < obj.length; i++) {
+						html += `<tr>
+								<td>${obj[i].oid}</td>
+								<td>${obj[i].sname}</td>
+								<td>${obj[i].sprice}</td>
+								<td>${obj[i].sdate}</td>
+								<td>${obj[i].status}</td>
+								<td>操作</td>
+							</tr>`;
+					}
+					$("#tbody").html(html);
+				}
+			}
+		});
+	})();
+
+	//左侧导航样式切换
+	$("#order div.left li").click(function (e) {
+		e.preventDefault();
+		$(this).addClass("active").siblings("li").removeClass("active");
+	});
+
 });
